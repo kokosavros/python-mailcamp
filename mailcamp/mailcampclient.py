b85f4a47d08db5369939d28e98f6f92d62434ca2
kokosavros <kokosavros@hotmail.com>
2018-03-22 13:24:46 +0100

    Added post method to the MailCampClient

--- mailcamp/mailcampclient.py
+++ mailcamp/mailcampclient.py
@@ -1,8 +1,11 @@
+import requests as re
+
+
 class MailCampClient:
     """
     MailCamp class to communicate with mailcamps XML API
     """
-    def __init__(self, username, xml_token, url='http://mailing.effectiveprofiles.com/xml.php'):
+    def __init__(self, username, xml_token, url):
         """
         Initialize the class with username, xml_token and url
         :param username: The username of a user in mailcamp
@@ -12,3 +15,11 @@ class MailCampClient:
         self.username = username
         self.xml_token = xml_token
         self.url = url
+
+    def _post(self, xml_request):
+        """
+        The post method to the MailCamp api
+        :param xml_request:
+        :return:
+        """
+        return re.post(self.url, data=xml_request)
