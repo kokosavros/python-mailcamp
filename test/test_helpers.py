b8be4ec2f587880273569a8c4568a0f8e5490f83
vermoudas <g.lampropoulos1987@gmail.com>
2018-03-23 14:09:57 +0100

    Added tests for the helper functions

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,5 +1,5 @@
 import unittest
-from mailcamp.helpers import dicttoxml
+from mailcamp.helpers import dicttoxml, xmltodict
 
 
 class TestHelpers(unittest.TestCase):
@@ -9,7 +9,152 @@ class TestHelpers(unittest.TestCase):
     def test_dicttoxml(self):
         # Test with empty dictionary
         result = dicttoxml({})
-        self.assertEquals(result, '', 'Empty dictionary does not return empty string')
+        self.assertEqual(
+            result, '', 'Empty dictionary does not return empty string')
+
+    def test_xmltodict(self):
+        xml = """<xmlrequest>
+                <status>success</status>
+                <message>You did it</message>
+                </xmlrequest>"""
+        result = xmltodict(xml)
+        expected = {'message': 'You did it', 'status': 'success'}
+        self.assertDictEqual(
+            expected,
+            result,
+            'The dictionary of a simple request is not correct')
+
+        xml = """<xmlrequest>
+                <status>success</status>
+                <message>You did it</message>
+                <details>
+                <name>name</name>
+                <lastname>lastname</lastname>
+                </details>
+                </xmlrequest>"""
+        result = xmltodict(xml)
+        expected = {
+            'message': 'You did it',
+            'details': {'name': 'name', 'lastname': 'lastname'},
+            'status': 'success'
+        }
+        self.assertDictEqual(
+            expected,
+            result,
+            'The dictionary of a request with simple nested field is '
+            'not correct')
+
+        xml = """<ids><id>1</id><id>2</id><id>3</id></ids>"""
+        result = xmltodict(xml)
+        expected = {
+            'id': ['1', '2', '3']
+        }
+        self.assertDictEqual(
+            expected,
+            result,
+            'The dictionary of a request with array field is '
+            'not correct')
+
+        xml = """<xmlrequest>
+                <status>success</status>
+                <message>You did it</message>
+                <details>
+                <item>
+                <name>name1</name>
+                <lastname>lastname1</lastname>
+                </item>
+                <item>
+                <name>name2</name>
+                <lastname>lastname2</lastname>
+                </item>
+                </details>
+                </xmlrequest>"""
+
+        result = xmltodict(xml)
+        expected = {
+            'message': 'You did it',
+            'status': 'success',
+            'details': {
+                'item': [
+                    {'name': 'name1', 'lastname': 'lastname1'},
+                    {'name': 'name2', 'lastname': 'lastname2'}
+                ]
+            }
+        }
+        self.assertDictEqual(
+            expected,
+            result,
+            'The dictionary of a request with array of nested fields is '
+            'not correct')
+
+        xml = """<xmlrequest>
+                <status>success</status>
+                <message>You did it</message>
+                <details>
+                <item>
+                <name>name1</name>
+                <lastname>lastname1</lastname>
+                </item>
+                <item>
+                <name>name2</name>
+                <lastname>lastname2</lastname>
+                </item>
+                <ids><id>1</id><id>2</id><id>3</id></ids>
+                </details>
+                </xmlrequest>"""
+        result = xmltodict(xml)
+        expected = {
+            'message': 'You did it',
+            'status': 'success',
+            'details': {
+                'item': [
+                    {'name': 'name1', 'lastname': 'lastname1'},
+                    {'name': 'name2', 'lastname': 'lastname2'}
+                ],
+                'ids': {'id': ['1', '2', '3']}
+            }
+        }
+        self.assertDictEqual(
+            expected,
+            result,
+            'The dictionary of a request with array of nested fields '
+            ' and simple array is not correct')
+
+        xml = """<xmlrequest>
+                <status>success</status>
+                <message>You did it</message>
+                <details>
+                <item>
+                <name>name1</name>
+                <lastname>lastname1</lastname>
+                </item>
+                <item>
+                <name>name2</name>
+                <lastname>lastname2</lastname>
+                </item>
+                <ids><id>1</id><id>
+                <active>True</active>
+                <number>2</number>
+                </id><id>3</id></ids>
+                </details>
+                </xmlrequest>"""
+        result = xmltodict(xml)
+        expected = {
+            'message': 'You did it',
+            'status': 'success',
+            'details': {
+                'item': [
+                    {'name': 'name1', 'lastname': 'lastname1'},
+                    {'name': 'name2', 'lastname': 'lastname2'}
+                ],
+                'ids': {'id': ['1', {'active': 'True', 'number': '2'}, '3']}
+            }
+        }
+        self.assertDictEqual(
+            expected,
+            result,
+            'The dictionary of a request with complex structure is not '
+            'correct')
 
 if __name__ == '__main__':
     unittest.main()
